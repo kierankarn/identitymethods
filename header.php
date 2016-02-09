d0f69d4faa6bb4be418fda509d35075edb67b779
kierankarn <kieran@tomango.co.uk>
2016-02-09 08:47:42 +0000

    Social and contact buttons added

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link href="https://fonts.googleapis.com/css?family=Lato:400,700,900" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="assets/style.css" media="screen">
+	<title>Home - Identity Methods</title>
+</head>
+<body>
+<header class="header">
+	<div class="container">
+		<a class="logo" href="#">Identity Methods</a>
+		<div class="social">
+			<a href="#"><i class="fa fa-twitter"></i></a>
+			<a href="#"><i class="fa fa-linkedin"></i></a>
+		</div>
+		<div class="vip">
+			<ul class="vip_area">
+				<li class="vip_buttons"><a href="#">VIP Member Registration</a></li>
+				<li class="vip_buttons"><a href="#">Contact Us</a></li>
+			</ul>
+		</div>
+	</div>
+</header>
