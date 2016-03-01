25b19f4fe2ef3e1a74d2c10325e864062fad104c
kierankarn <kieran@tomango.co.uk>
2016-03-01 19:03:15 +0000

    Worked on Tabs

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
 <?php include 'header.php';?>
-
-
+<main class="wrapper">
+<div class="tabs">
+	<div class="tabs-headers">
+			<a class="tabs-active" href="#">Solutions</a>
+			<a href="#">Professional Services</a>
+			<a href="#">Products</a>
+	</div> 
+	<div class="tabs-content">
+		<div class="tabs-active">
+		</div>
+		<div class="tab">
+		</div>
+		<div class="tab">
+		</div>
+	</div>
+</div>
+</main>
 <?php include 'footer.php'; ?>
