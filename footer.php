f89dbd007a22202237e836e5b45ac5874b34456c
kierankarn <kieran@tomango.co.uk>
2016-03-10 09:12:02 +0000

    Added Footer
    Still need to sort out navigation

--- footer.php
+++ footer.php
@@ -1,4 +1,51 @@
-
-
+<div class="nav-low">
+	<div class="wrap">
+	<ul class="menu">
+		<li><a href="#">Solve your problem</a></li>
+		<li ><a href="#">Methods</a>
+			<ul class="sub-menu">
+				<li><a href="#">Identity and Access Management</a></li>
+				<li><a href="#">Data Security and Privacy</a></li>
+				<li><a href="#">Cyber Threat</a></li>
+			</ul>
+		</li>
+		<li><a href="#">Professional Services</a>
+			<ul class="sub-menu">
+				<li><a href="#">Architecture and Design</a></li>
+				<li><a href="#">Business Analysis</a></li>
+				<li><a href="#">Project and Programme Management</a></li>
+				<li><a href="#">Strategic Advisory</a></li>
+				<li><a href="#">Technical Delivery</a></li>
+			</ul>
+		</li>
+		<li><a href="#">Products</a>
+			<ul class="sub-menu">
+				<li><a href="#">IBM</a></li> 
+				<li><a href="#">Camouflage</a></li>
+				<li><a href="#">Secure Auth</a></li>
+				<li><a href="#">SMS Passcode</a></li>
+				<li><a href="#">Centrify</a></li>
+				<li><a href="#">ForgeRock</a></li>
+				<li><a href="#">CyberArk</a></li>
+				<li><a href="#">Beta Systems</a></li>
+			</ul>
+		</li>
+	</ul>
+	</div>
+</div>
+<footer>
+	<div class="wrap">
+		<div class="social-footer">
+			<a href="#"><i class="fa fa-twitter"></i></a>
+			<a href="#"><i class="fa fa-linkedin"></i></a>
+		</div>
+		© 2016 Identity Methods Ltd. All rights reserved. 
+		<ul class="footer-menu">
+		<li><a href="#">Privacy Policy &amp; Terms and Conditions</a></li>
+		</ul>
+		<span><a href="http://www.tomango.co.uk/creates/web/design/">Website design in Sussex</a> - Tomango</span><br />
+		<small>Registered Address: 168 Church Road, Hove, East Sussex, BN3 2DL. Registered No. 07867649</small>
+	</div>
+</footer>
 </body>
 </html>
